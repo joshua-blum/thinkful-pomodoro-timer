575b10dfeeb14c02febf88d59538da5eacc3922d
Joshua Blum <blum.r.joshua@gmail.com>
2021-07-20 16:31:51 -0700

    Updates

---
+++ precache-manifest.2ed279be27bd267e46f2d355641726dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3005d08f59c6a767d712d7230841a1",
+    "url": "/thinkful-pomodoro-timer/index.html"
+  },
+  {
+    "revision": "b27f877a4499cbcf10de",
+    "url": "/thinkful-pomodoro-timer/static/css/main.1e64e2e4.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/thinkful-pomodoro-timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/thinkful-pomodoro-timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27f877a4499cbcf10de",
+    "url": "/thinkful-pomodoro-timer/static/js/main.f0ba7b27.chunk.js"
+  },
+  {
+    "revision": "89cc673d493d92224a13",
+    "url": "/thinkful-pomodoro-timer/static/js/runtime-main.8c188555.js"
+  }
+]);
